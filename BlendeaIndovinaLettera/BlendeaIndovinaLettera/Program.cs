53ca47b4617dd53d549931ad385bb63e14d9f4ac
BlendeaMihai <98150787+BlendeaMihai@users.noreply.github.com>
2022-02-18 10:37:02 +0100

    IndovinaNumero

--- BlendeaIndovinaLettera/BlendeaIndovinaLettera/Program.cs
+++ BlendeaIndovinaLettera/BlendeaIndovinaLettera/Program.cs
@@ -6,7 +6,39 @@ namespace BlendeaIndovinaLettera
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Random Num = new Random();
+            int num = Num.Next(1, 100);
+            int tentativi = 0;
+            int n;
+            int punteggio = 1000;
+            
+            Console.WriteLine("Srivi un numero compreso tra 1 e 100");
+            while (tentativi <= 100)
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+                tentativi++;
+                if (num < n)
+                {
+                  
+                    Console.WriteLine("Il numero generato è piu basso");
+                }
+                else if (num > n)
+                {
+                    
+                    Console.WriteLine("Il numero generato è piu grande");
+                }
+                else if (num == n)
+                {
+                    Console.WriteLine($"Bravo hai indovinato il tuo numero è {num}");
+                    Console.WriteLine($"Il numero dei tuoi tentativi è: {tentativi}");
+                    Console.WriteLine($"Il tuo punteggio è {punteggio - tentativi * 50}");
+                 
+                }
+             
+            }
+        
+
+            
+       }
     }
 }
